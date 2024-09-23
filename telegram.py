0129fcd0b2c91acbb22fe833a9a9d63717ddbaa0
dimitrovdv <dimitrovdv@bk.ru>
2024-09-23 17:41:00 +0300

    fix telegram

--- telegram.py
+++ telegram.py
@@ -1,8 +1,9 @@
 import sys
 import requests
-ACCOUNT = sys.argv[1]
-bottoken = sys.argv[2]
+# ACCOUNT = sys.argv[1]
+
+TOKEN = "6815228522:AAFl1umxHLB0DCNSf0D3RDdiXPbUfr1Sc7E"
 chat_id = "729044367"
-message = f"Задание успешно выполнено для аккаунта: {ACCOUNT}!"
-url = f"https://api.telegram.org/bot{bottoken}/sendMessage?chat_id={chat_id}&text={message}"
+message = "Задание успешно выполнено!"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 print(requests.get(url).json())
