33bf247bb5e1c3dad944a6eaa87a514331130313
dimitrovdv <dimitrovdv@bk.ru>
2024-09-24 11:58:38 +0300

    fix telegram sent

--- telegram.py
+++ telegram.py
@@ -1,9 +1,16 @@
-import sys
 import requests
-# ACCOUNT = sys.argv[1]
+import os
+from dotenv import load_dotenv
+load_dotenv()
+apple_id = os.getenv('apple_id')
+bundle_id = os.getenv('bundle_id')
+appid = os.getenv('appid')
+print(apple_id)
+print(bundle_id)
+print(appid)
 
 TOKEN = "6815228522:AAFl1umxHLB0DCNSf0D3RDdiXPbUfr1Sc7E"
 chat_id = "729044367"
-message = "Задание успешно выполнено!"
+message = f"Задание для УЗ {apple_id} успешно выполнено! Куплено приложение: {bundle_id};{appid}!"
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 print(requests.get(url).json())
